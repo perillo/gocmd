1cfde9e255142b971fb564176f65c9059a74362a
Manlio Perillo <manlio.perillo@gmail.com>
2020-01-30 00:07:47 +0100

    cmd/gocmd: make gocmd an useful debug tool
    Reimplement the gocmd command to make it useful for debugging the
    invoke.Go function.
    
    Enable the GOCMDDEBUG variable to debug corner cases.
    Use a custom writer for stdout, stderr and standard logging, and make it
    print an STDOUT, STDERR and LOG prefix, respectively, for each line.

--- cmd/gocmd/main.go
+++ cmd/gocmd/main.go
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Command gocmd is used for debugging the invoke.Go function.
+// The output from stdout, stderr and the standard log is redirected to stdout,
+// and each line is printed with a prefix indicating the origin.
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"log"
 	"os"
 	"strings"
@@ -13,42 +16,56 @@ import (
 	"github.com/perillo/gocmd/internal/invoke"
 )
 
-const (
-	prefix = "STDERR: "
-	indent = "    "
+var (
+	tstdout *writer = &writer{prefix: "STDOUT "}
+	tstderr *writer = &writer{prefix: "STDERR "}
+	tlog    *writer = &writer{prefix: "LOG    "}
 )
 
 func main() {
+	// Set the GOCMDDEBUG environment variable to debug some corner cases.
+	os.Setenv("GOCMDDEBUG", "on")
+
 	log.SetFlags(0)
-	log.SetPrefix("STDERR: ")
+	log.SetOutput(tlog)
 	if len(os.Args) == 1 {
 		return
 	}
 
 	stdout, err := invoke.Go(os.Args[1], os.Args[2:], nil)
 	if err != nil {
-		log.Print(indented(err, prefix, indent))
+		tstderr.WriteString(err.Error())
 	}
 	if len(stdout) != 0 {
-		fmt.Printf("%s\n", stdout)
+		tstdout.Write(stdout)
 	}
 }
 
-func indented(err error, prefix, indent string) string {
-	var b strings.Builder
+// writer writes on stdout each line with the specified prefix.
+type writer struct {
+	prefix string
+}
 
-	for i, line := range strings.Split(err.Error(), "\n") {
-		if line == "" {
-			continue
-		}
-		if i > 0 {
-			b.WriteString(prefix)
-			b.WriteString(indent)
-		}
+func (w *writer) Write(buf []byte) (int, error) {
+	var b bytes.Buffer
 
+	for _, line := range bytes.Split(buf, []byte("\n")) {
+		b.WriteString(w.prefix)
+		b.Write(line)
+		b.WriteByte('\n')
+	}
+
+	return os.Stdout.Write(b.Bytes())
+}
+
+func (w *writer) WriteString(buf string) (int, error) {
+	var b bytes.Buffer
+
+	for _, line := range strings.Split(buf, "\n") {
+		b.WriteString(w.prefix)
 		b.WriteString(line)
 		b.WriteByte('\n')
 	}
 
-	return b.String()
+	return os.Stdout.Write(b.Bytes())
 }
