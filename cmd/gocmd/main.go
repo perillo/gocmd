1554e0c62f099d9d74fa772ad31a42b5d1cb9107
Manlio Perillo <manlio.perillo@gmail.com>
2020-01-30 12:00:47 +0100

    cmd/gocmd: move debug support to internal/debug package
    Move the debug support code from cmd/gocmd to the internal/debug
    package.
    Rename the prefix LOG to STDLOG, for consistency with STDOUT and STDERR.

--- cmd/gocmd/main.go
+++ cmd/gocmd/main.go
@@ -8,20 +8,11 @@
 package main
 
 import (
-	"bytes"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 
+	"github.com/perillo/gocmd/internal/debug"
 	"github.com/perillo/gocmd/internal/invoke"
-)
-
-var (
-	tstdout *writer = &writer{prefix: "STDOUT "}
-	tstderr *writer = &writer{prefix: "STDERR "}
-	tlog    *writer = &writer{prefix: "LOG    "}
-	gopath          = thegopath()
 )
 
 func main() {
@@ -29,65 +20,16 @@ func main() {
 	os.Setenv("GOCMDDEBUG", "on")
 
 	log.SetFlags(0)
-	log.SetOutput(tlog)
+	log.SetOutput(debug.Stdlog)
 	if len(os.Args) == 1 {
 		return
 	}
 
 	stdout, err := invoke.Go(os.Args[1], os.Args[2:], nil)
 	if err != nil {
-		tstderr.WriteString(err.Error())
+		debug.Stderr.WriteString(err.Error())
 	}
 	if stdout != nil {
-		tstdout.Write(stdout)
+		debug.Stdout.Write(stdout)
 	}
-}
-
-// writer writes on stdout each line with the specified prefix.
-type writer struct {
-	prefix string
-}
-
-func (w *writer) Write(buf []byte) (int, error) {
-	var b bytes.Buffer
-
-	for _, line := range bytes.Split(buf, []byte("\n")) {
-		b.WriteString(w.prefix)
-		b.Write(line)
-		b.WriteByte('\n')
-	}
-
-	return w.emit(b)
-}
-
-func (w *writer) WriteString(buf string) (int, error) {
-	var b bytes.Buffer
-
-	for _, line := range strings.Split(buf, "\n") {
-		b.WriteString(w.prefix)
-		b.WriteString(line)
-		b.WriteByte('\n')
-	}
-
-	return w.emit(b)
-}
-
-func (w *writer) emit(b bytes.Buffer) (int, error) {
-	// Make any path inside $GOPATH relative to $GOPATH.
-	buf := b.Bytes()
-	if gopath != nil {
-		buf = bytes.Replace(buf, gopath, []byte("$gopath"), -1)
-	}
-
-	return os.Stdout.Write(buf)
-}
-
-func thegopath() []byte {
-	path := os.Getenv("GOPATH")
-	if path == "" {
-		return nil
-	}
-	list := filepath.SplitList(path)
-
-	return []byte(list[0])
 }
---
+++ internal/debug/debug.go
@@ -0,0 +1,79 @@
+// Copyright 2020 Manlio Perillo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package debug provides support for debugging the invoke.Go function.
+// It provides a custom Stdout, Stderr and Stdlog that will prefix each line
+// with the string "STDOUT", "STDERR" and "STDLOG" respectively, in order to
+// multiplex the output from stdout, stderr and the standard log package to
+// stdout.
+// It will also make all the absolute paths read from cmd/go relative to
+// $GOPATH.
+package debug
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var gopath = thegopath()
+
+// Stdout, Stderr and Stdlog are io.Writer that allow to multiplex output
+// directed to stdout, stderr or logged by the standard log package to stdout.
+var (
+	Stdout = &Writer{prefix: "STDOUT "}
+	Stderr = &Writer{prefix: "STDERR "}
+	Stdlog = &Writer{prefix: "STDLOG "}
+)
+
+// Writer writes on stdout each line with the specified prefix.
+type Writer struct {
+	prefix string
+}
+
+// Write implements the Read interface.
+func (w *Writer) Write(buf []byte) (int, error) {
+	var b bytes.Buffer
+
+	for _, line := range bytes.Split(buf, []byte("\n")) {
+		b.WriteString(w.prefix)
+		b.Write(line)
+		b.WriteByte('\n')
+	}
+
+	return w.emit(b)
+}
+
+func (w *Writer) WriteString(buf string) (int, error) {
+	var b bytes.Buffer
+
+	for _, line := range strings.Split(buf, "\n") {
+		b.WriteString(w.prefix)
+		b.WriteString(line)
+		b.WriteByte('\n')
+	}
+
+	return w.emit(b)
+}
+
+func (w *Writer) emit(b bytes.Buffer) (int, error) {
+	// Make any path inside $GOPATH relative to $GOPATH.
+	buf := b.Bytes()
+	if gopath != nil {
+		buf = bytes.Replace(buf, gopath, []byte("$gopath"), -1)
+	}
+
+	return os.Stdout.Write(buf)
+}
+
+func thegopath() []byte {
+	path := os.Getenv("GOPATH")
+	if path == "" {
+		return nil
+	}
+	list := filepath.SplitList(path)
+
+	return []byte(list[0])
+}
