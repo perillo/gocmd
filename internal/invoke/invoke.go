053f72d4ed378cb96d462a96882f94927aca9c85
Manlio Perillo <manlio.perillo@gmail.com>
2020-02-03 18:07:24 +0100

    invoke: introduce a custom Error type
    Implement a custom Error type to make error handling more convenient for
    clients of the invoke.Go function.
    
    Update the Go function documentation to document that the error returned
    will be of type *Error.

--- internal/invoke/invoke.go
+++ internal/invoke/invoke.go
@@ -29,13 +29,37 @@ type Attr struct {
 	Dir string
 }
 
+// Error is returned by Go in case the go command returns an error.
+type Error struct {
+	Argv   []string // arguments to the go command
+	Stderr []byte   // the entire content of the go command stderr
+	Err    error    // the original error from exec.Command.Run
+}
+
+// Error implements the error interface.
+func (e *Error) Error() string {
+	argv := strings.Trim(fmt.Sprint(e.Argv), "[]")
+	stderr := string(e.Stderr)
+	msg := "go " + argv + ": " + e.Err.Error()
+
+	if stderr == "" {
+		return msg
+	}
+
+	return msg + ": " + stderr
+}
+
+// Unwrap implements the error unwrapping interface.
+func (e *Error) Unwrap() error {
+	return e.Err
+}
+
 // Go invokes a cmd/go command and returns its stdout content and an error.  It
 // implicitly assumes that the cmd/go command is invoked with the -json flag
 // set.
 //
 // If the cmd/go command returns a non 0 exit status, Go will return the stdout
-// content, or nil if empty, and the error as returned by the exec package,
-// with the stderr content as additional context.
+// content, or nil if empty, and the error will be of type *Error.
 //
 // If the cmd/go command returns a 0 exit status, Go will return the possibly
 // empty stdout content and a nil error.  The stderr content will be ignored,
@@ -58,16 +82,17 @@ func Go(verb string, argv []string, attr *Attr) ([]byte, error) {
 		// Just return the error, including the non empty stderr output as is.
 		// Make sure to also return the stdout content, since it may be
 		// important.  But only if it is not empty.
-		argv := strings.Trim(fmt.Sprint(argv), "[]")
 		var buf []byte
 		if stdout.Len() > 0 {
 			buf = stdout.Bytes()
 		}
-		if stderr.Len() > 0 {
-			return buf, fmt.Errorf("go %v: %w: %s", argv, err, stderr)
+		err := &Error{
+			Argv:   argv,
+			Stderr: stderr.Bytes(),
+			Err:    err,
 		}
 
-		return buf, fmt.Errorf("go %v: %w", argv, err)
+		return buf, err
 	}
 	if stderr.Len() != 0 && os.Getenv("GOCMDDEBUG") != "" {
 		argv := strings.Trim(fmt.Sprint(argv), "[]")
